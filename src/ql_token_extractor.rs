9813543872175d502ee4ec270dc7bae171a57082
RomanSavh <RomanSavh@users.noreply.github.com>
2025-02-19 13:10:06 +0200

    token improvements

--- src/ql_token_extractor.rs
+++ src/ql_token_extractor.rs
@@ -2,13 +2,22 @@ use axum_extra::{
     headers::{authorization::Bearer, Authorization},
     TypedHeader,
 };
-use yft_service_sdk::external::axum::{
-    extract::FromRequestParts,
-    http::{request::Parts, StatusCode},
-    RequestPartsExt,
+use yft_service_sdk::external::{
+    async_graphql::{self, Error},
+    axum::{
+        extract::FromRequestParts,
+        http::{request::Parts, StatusCode},
+        RequestPartsExt,
+    },
 };
 
 pub struct ExtractQlBearerToken(pub Option<String>);
+
+impl ExtractQlBearerToken {
+    pub fn get_token(&self) -> async_graphql::Result<String> {
+        self.0.clone().ok_or(Error::new("InvalidToken"))
+    }
+}
 
 impl<S> FromRequestParts<S> for ExtractQlBearerToken
 where
@@ -17,9 +26,7 @@ where
     type Rejection = (StatusCode, String);
 
     async fn from_request_parts(parts: &mut Parts, _: &S) -> Result<Self, Self::Rejection> {
-        let header = parts
-            .extract::<TypedHeader<Authorization<Bearer>>>()
-            .await;
+        let header = parts.extract::<TypedHeader<Authorization<Bearer>>>().await;
 
         let Ok(token) = header else {
             return Ok(ExtractQlBearerToken(None));
